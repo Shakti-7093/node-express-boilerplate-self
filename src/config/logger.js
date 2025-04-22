2ce24c5fc9159d56bae7ebd89c895aa55d9ced01
sahdev <sahdev@apptodate.co.il>
2025-04-22 15:52:28 +0530

    updated logger and changed to daily log file max size 14mb and remove file after 14 days

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,3 +1,5 @@
+const path = require('path');
+const DailyRotateFile = require('winston-daily-rotate-file');
 const winston = require('winston');
 const config = require('./config');
 
@@ -8,17 +10,29 @@ const enumerateErrorFormat = winston.format((info) => {
   return info;
 });
 
+const logFormat = winston.format.printf(({ level, message, timestamp }) => {
+  return `${timestamp} ${level}: ${message}`;
+});
+
 const logger = winston.createLogger({
   level: config.env === 'development' ? 'debug' : 'info',
   format: winston.format.combine(
     enumerateErrorFormat(),
-    config.env === 'development' ? winston.format.colorize() : winston.format.uncolorize(),
-    winston.format.splat(),
-    winston.format.printf(({ level, message }) => `${level}: ${message}`)
+    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    winston.format.splat()
   ),
   transports: [
     new winston.transports.Console({
       stderrLevels: ['error'],
+      format: winston.format.combine(winston.format.colorize(), logFormat),
+    }),
+    new DailyRotateFile({
+      filename: path.join(__dirname, '../../storage/logs/server-%DATE%.log'),
+      datePattern: 'YYYY-MM-DD',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+      format: winston.format.combine(winston.format.uncolorize(), logFormat),
     }),
   ],
 });
